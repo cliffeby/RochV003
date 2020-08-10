3071d8bc42467d00b6b9570c1e1752d15087b4f7
cliff eby <clifford.eby@gmail.com>
2020-08-09 23:45:20 -0400

    commented unused validators

--- src/app/services/validation.service.ts
+++ src/app/services/validation.service.ts
@@ -95,40 +95,40 @@ export class ValidationService {
     }
   }
 
-  static emailValidator(control) {
-    // RFC 2822 compliant regex
-    if (control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
-      return null;
-    } else {
-      return { 'invalidEmailAddress': true };
-    }
-  }
-  static nameValidator(control) {
-    // RFC 2822 compliant regex
-    if (control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
-      return null;
-    } else {
-      return { 'invalidName': true };
-    }
-  }
+  // static emailValidator(control) {
+  //   // RFC 2822 compliant regex
+  //   if (control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+  //     return null;
+  //   } else {
+  //     return { 'invalidEmailAddress': true };
+  //   }
+  // }
+  // static nameValidator(control) {
+  //   // RFC 2822 compliant regex
+  //   if (control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+  //     return null;
+  //   } else {
+  //     return { 'invalidName': true };
+  //   }
+  // }
 
-  static dateValidator(control) {
-    // console.log('ConditionpreCheck', control.value);
-    if (control.value == !null) {
-      return null;
-        } else {
-      console.log('ConditionInvaildDate', control.value);
-      return { 'invalidDate': true };
-    }
-  }
+  // static dateValidator(control) {
+  //   // console.log('ConditionpreCheck', control.value);
+  //   if (control.value == !null) {
+  //     return null;
+  //       } else {
+  //     console.log('ConditionInvaildDate', control.value);
+  //     return { 'invalidDate': true };
+  //   }
+  // }
 
-  static passwordValidator(control) {
-    // {6,100}           - Assert password is between 6 and 100 characters
-    // (?=.*[0-9])       - Assert a string has at least one number
-    if (control.value.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
-      return null;
-    } else {
-      return { 'invalidPassword': true };
-    }
-  }
+  // static passwordValidator(control) {
+  //   // {6,100}           - Assert password is between 6 and 100 characters
+  //   // (?=.*[0-9])       - Assert a string has at least one number
+  //   if (control.value.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
+  //     return null;
+  //   } else {
+  //     return { 'invalidPassword': true };
+  //   }
+  // }
 }
